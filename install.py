d03b0aaf82df0970e23962a0fd0c0e1f91862d0c
toxic <filip.herle@gmail.com>
2017-03-15 20:34:17 -0700

    Update install.py

--- install.py
+++ install.py
@@ -28,13 +28,20 @@ if option == 1:
     print "\033[1;33m[*] Loading...\033[0m"
     os.system('apt-get install python-pip')
     import pip
+    os.system('sudo apt-get install libjpeg-dev libfreetype6 zlib1g-dev')
     os.system('pip install google')
+    os.system('pip install pythonwhois')
+    os.system('sudo apt-get install libjpeg-dev libfreetype6 zlib1g-dev')
+    os.system('pip install PIL')
+    os.system('pip install pillow')
+    #os.system('pip install PIL')
+    os.system('pip install http://effbot.org/downloads/Imaging-1.1.6.tar.gz')
     os.system('pip install netifaces')
     install = os.system("apt-get update && apt-get install -y build-essential git")
     install2 = os.system("cp -R trity/ /opt/ && cp trity.py /opt/trity && cp run.sh /opt/trity && cp run.sh /usr/bin/trity && chmod +x /usr/bin/trity")
     os.system('apt-get install sendemail')
     os.system('apt-get install libncurses5')
-    pip.main(["install", "netifaces", "scapy", "SpoofMAC", "pythonwhois", "readline", "BeautifulSoup", "requests", "mechanize", "google", "qrcode"])
+    pip.main(["install", "netifaces", "PIL", "scapy", "SpoofMAC", "pythonwhois", "readline", "BeautifulSoup", "requests", "mechanize", "google", "qrcode"])
     print "\033[1;32m[!] Finished Installing! Run 'trity' to run program [!]\033[0m"
     sys.exit()
 else:
